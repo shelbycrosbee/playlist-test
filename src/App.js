7e2e1345cc98d59e215d2ac2b864a655de2030dd
Shelby Miller <shelbycrosbee@gmail.com>
2019-08-11 08:42:17 -0600

    initial push

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Playlist from './Playlist'
+import { tsPropertySignature } from '@babel/types';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component{
+
+  
+
+  return (){
+    render(
+    <div>
+        <Playlist /> 
     </div>
   );
 }
-
+}
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { SpotifyApiContext } from 'react-spotify-api';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<SpotifyApiContext.Provider value='Bearer BQCbkaoh8G-usRH103A9VyguBlzI0xyRAZ7p-eVqFlKUpwQDvbUFCCP7uBDC69Nn1LMhosa_IR0Ypvpbn126xOyKmKBltFX7GUASO7NxgRaAKmC1qEoY8XKmn_nNN8cAlPABBNsH-R0G9fOsr3YZObfRkdcZhofPwdYP1g'><App /></SpotifyApiContext.Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
