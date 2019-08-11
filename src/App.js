eae8e42e5765ae52383c09c855be0ef88f0fe3e4
Shelby Miller <shelbycrosbee@gmail.com>
2019-08-11 10:15:22 -0600

    api call for authorization corrections

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
 import React from 'react';
 import './App.css';
 // import Playlist from './Playlist'
-import { tsPropertySignature } from '@babel/types';
 import axios from 'axios'
-import { CodeGenerator } from '@babel/generator';
 
 
 
 const CLIENT_ID = '1af7ab55e7e64df992dadf08719596ac';
 const CLIENT_SECRET = '40d2b6c4973444328b30f84a871dcce9';
+
 export const authEndpoint = 'https://accounts.spotify.com/authorize';
-// Replace with your app's client ID, redirect URI and desired scopes
-const clientId = "1af7ab55e7e64df992dadf08719596ac";
+const clientId = CLIENT_ID;
 const redirectUri = "http://localhost:3000/";
 const scopes = [
   "user-read-currently-playing",
   "user-read-playback-state",
+  'user-read-email',
+  'user-read-private'
 ];
-// Get the hash of the url
 const hash = window.location.hash
   .substring(1)
   .split("&")
-  .reduce(function(initial, item) {
+  .reduce(function (initial, item) {
     if (item) {
       var parts = item.split("=");
       initial[parts[0]] = decodeURIComponent(parts[1]);
@@ -31,61 +30,58 @@ const hash = window.location.hash
 window.location.hash = "";
 
 
-class App extends React.Component{
-  constructor(props){
+class App extends React.Component {
+  constructor(props) {
     super(props)
     this.state = {
-      song: ''
+      token: ''
     }
   }
   componentDidMount() {
-    // Set token
+
     let _token = hash.access_token;
     if (_token) {
-      // Set token
       this.setState({
         token: _token
       });
     }
+
+   axios.get(
+      'https://api.spotify.com/v1/users/e3itsm9xo2go4zn5j2lgmkg5g', {
+      headers: {
+        Authorization: `Bearer ${_token}`
+      }
+    }
+    )
+      .then(response => {
+        console.log(response)
+      
+      })
+
+      .catch(error => {
+        console.log(error)
+      })
+  
   }
-
-
-async componentDidMount(){
-  await axios.get(
-    'https://api.spotify.com/v1/users/22f63oznp4huleaxulv63sjxi',
-    {
-    //   headers: {
-    //     Authorization: 'Bearer BQAp2HbDRE3nDCTY-V0xUfDjIT--97sSVEvtVKWhmdgb-nviEPe290Tbj1JSHRk7NV9oF5DVW943CTTu3o2O8WFSS4gRVvz9Kv_cXbMllJPV4MbIjypTN9e9gwLuwrwL4ShoR8E2q7NGVEIxXwS7znwlUtemSIj_A26O43I9'
-    //   }
-     }
-  )
-  .then(response => {
-    console.log(response)
-  }) 
-  .catch(error => {
-    console.log(error)
-  })
-}
-
-  render (){
-    return(
+  render() {
+    return (
       <div className="App">
-      <header className="App-header">
-     
-      {!this.state.token && (
-        <a
-          className="btn btn--loginApp-link"
-          href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`}
-        >
-          Login to Spotify
+        <header className="App-header">
+
+          {!this.state.token && (
+            <a
+              className="btn btn--loginApp-link"
+              href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`}
+            >
+              Login to Spotify
         </a>
-      )}
-      {/* {this.state.token && (
+          )}
+          {/* {this.state.token && (
         // Spotify Player Will Go Here In the Next Step
       )} */}
-      </header>
-    </div>
-  );
-}
+        </header>
+      </div>
+    );
+  }
 }
 export default App;
