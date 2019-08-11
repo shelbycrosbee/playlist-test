d3f7cca40640ec783983027d9430ae5463eb2f64
Shelby Miller <shelbycrosbee@gmail.com>
2019-08-11 09:48:32 -0600

    login success

--- src/App.js
+++ src/App.js
@@ -3,6 +3,33 @@ import './App.css';
 // import Playlist from './Playlist'
 import { tsPropertySignature } from '@babel/types';
 import axios from 'axios'
+import { CodeGenerator } from '@babel/generator';
+
+
+
+const CLIENT_ID = '1af7ab55e7e64df992dadf08719596ac';
+const CLIENT_SECRET = '40d2b6c4973444328b30f84a871dcce9';
+export const authEndpoint = 'https://accounts.spotify.com/authorize';
+// Replace with your app's client ID, redirect URI and desired scopes
+const clientId = "1af7ab55e7e64df992dadf08719596ac";
+const redirectUri = "http://localhost:3000/";
+const scopes = [
+  "user-read-currently-playing",
+  "user-read-playback-state",
+];
+// Get the hash of the url
+const hash = window.location.hash
+  .substring(1)
+  .split("&")
+  .reduce(function(initial, item) {
+    if (item) {
+      var parts = item.split("=");
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+    }
+    return initial;
+  }, {});
+window.location.hash = "";
+
 
 class App extends React.Component{
   constructor(props){
@@ -11,35 +38,19 @@ class App extends React.Component{
       song: ''
     }
   }
+  componentDidMount() {
+    // Set token
+    let _token = hash.access_token;
+    if (_token) {
+      // Set token
+      this.setState({
+        token: _token
+      });
+    }
+  }
 
 
 async componentDidMount(){
-
-const CLIENT_ID = '1af7ab55e7e64df992dadf08719596ac';
-const CLIENT_SECRET = '40d2b6c4973444328b30f84a871dcce9'
-
-await axios.post({
-  url: `https://accounts.spotify.com/api/token`,
-  data: { // in axios data is the body request
-    grant_type: 'authorization_code',
-    code: req.query.code, // code I'm receiving from https://accounts.spotify.com/authorize
-    redirect_uri: 'http://localhost:3000/'
-  },
-  headers: {
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'Authorization': 'Basic ' + Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64') // client id and secret from env
-  }
-}
-)
-.then(response => {
-  console.log(response)
-}) 
-.catch(error => {
-  console.log(error)
-})
-
-
-
   await axios.get(
     'https://api.spotify.com/v1/users/22f63oznp4huleaxulv63sjxi',
     {
@@ -58,9 +69,21 @@ await axios.post({
 
   render (){
     return(
-    <div>
-        Playlist Test
-
+      <div className="App">
+      <header className="App-header">
+     
+      {!this.state.token && (
+        <a
+          className="btn btn--loginApp-link"
+          href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`}
+        >
+          Login to Spotify
+        </a>
+      )}
+      {/* {this.state.token && (
+        // Spotify Player Will Go Here In the Next Step
+      )} */}
+      </header>
     </div>
   );
 }
